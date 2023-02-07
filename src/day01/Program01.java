5341e8b614fe88789db1c9b9a3e9f33b608056d9
harinigannu <46520474+harinigannu@users.noreply.github.com>
2023-02-07 20:22:03 +0530

    day02
    java basics

--- src/day01/Program01.java
+++ src/day01/Program01.java
@@ -2,8 +2,26 @@ package day01;
 
 public class Program01 {
 
-	public static void main(String[] args) {
+	public static void fun() {
 		// TODO Auto-generated method stub
+		int x = 102345;
+		char y = 'a';
+		boolean a = true;
+		byte b= 118;
+		short c=32000;
+		long d=1234567890;
+		double a1;
+		a1=12345.67890d;
+		float a2=12345.67f;
+		System.out.println(a2);
+		System.out.println(a1);
+		System.out.println(d);
+		System.out.println("Good Morning");
+		System.out.println(x*x);
+	}
+	public static void main(String[] args) {
+		System.out.println("good");
+		fun();
 
 	}
 
--- src/day02/Program01.java
+++ src/day02/Program01.java
@@ -2,9 +2,17 @@ package day02;
 
 public class Program01 {
 
-	public static void main(String[] args) {
+	public static int fun() {
 		// TODO Auto-generated method stub
-
-	}
-
+		int a = 15;
+		System.out.println(a*a);
+		return a*a;
 }
+    public static void main(String[] args) {
+	long b = fun();
+	System.out.println(b+b);
+    }
+}
+//here function solution is 15*15=225
+//and long solution is function + function = 225+225 = 240
+//because in above program we declared long b is equal to fun();
