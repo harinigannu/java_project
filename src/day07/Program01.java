5b6a9c599969d940a2af8afc06aad0f0a5faaed5
harinigannu <46520474+harinigannu@users.noreply.github.com>
2023-02-13 08:39:33 +0530

    day07

---
+++ src/day07/Program01.java
@@ -0,0 +1,17 @@
+package day07;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=1;
+		if(a>b)
+		{
+			System.out.println("good day...!");
+			System.out.println("good morning...!");
+
+		}
+
+	}
+
+}
