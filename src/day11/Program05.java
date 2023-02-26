748a62a9f36857226b7b5222c8dc495151392304
harinigannu <46520474+harinigannu@users.noreply.github.com>
2023-02-26 13:14:54 +0530

    day11

---
+++ src/day11/Program05.java
@@ -0,0 +1,26 @@
+package day11;
+
+public class Program05 {
+
+	public static void main(String[] args) {
+		
+		for(int i=-5;i<=-1;i++)
+		{
+		for(int j=-1;j>=i;j--)
+		{
+			System.out.print(j);
+		}
+		System.out.println();
+		}
+		
+		/*for(int i=-1;i<=-5;i++)//1 2 3 4 5
+		{
+		for(int j=-5;j>=i;j--)
+		{
+			System.out.print(j);
+		}
+		System.out.println();
+		}*/
+	}
+}
+		
