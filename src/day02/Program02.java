e92a21ecf8c2b07069b9dd80c89dca64e56953dc
harinigannu <46520474+harinigannu@users.noreply.github.com>
2023-03-06 20:17:59 +0530

    day02

--- src/day02/Program02.java
+++ src/day02/Program02.java
@@ -15,6 +15,8 @@ public class Program02 {
 	}
 }
 
+
+
 //we can give any name in place of cal and this method is used only for integer of 2 values only and we can use this method N number of times.
 //here we used a method in line 5, compiler will search for the cal method, and it will directly assign 20 to x and 40 to y.
 //here we used a method in line 6, compiler will search for the cal method, and it will directly assign 10 to x and 30 to y.
